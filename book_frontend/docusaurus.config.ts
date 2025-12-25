f64e82a10f72fc97747bcae0a84b832ef93f6f7c
tayyabahussain98 <tayyabahussain98@users.noreply.github.com>
2025-12-25 16:42:01 +0500

    Update docusaurus.config.ts

--- book_frontend/docusaurus.config.ts
+++ book_frontend/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://tayyabahussain98.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI-book-creation-with-spec-driven-development/',
+  baseUrl: '/AI-Book-Creation-With-Spec-Driven-Development/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tayyabahussain98', // Usually your GitHub org/user name.
-  projectName: 'AI-book-creation-with-spec-driven-development', // Usually your repo name.
+  projectName: 'AI-Book-Creation-With-Spec-Driven-Development', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
